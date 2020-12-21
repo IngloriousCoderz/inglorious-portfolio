f7409cb389a17fbd1a709e7d5b0872f32bc57eec
IceOnFire <antony.mistretta@gmail.com>
2020-12-21 14:00:56 +0100

    End CSS section and start with JS

---
+++ 06-js-fundamentals/hello-world.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * This is a hello world application that shows alerts.
+ */
+
+/* this shows a popup with the number 6 */
+// alert(3 + 1 + 2); // the sum will be performed before the alert
+
+// alert('Hello world!');
+
+/* declaring a variable */
+let message;
+
+/* assign a value to the variable */
+message = 'Hello world!';
+
+let message = 'Hello world!';
+message = 'Goodbye world...';
+
+/* referencing the variable */
+alert(message);
+alert(message);
